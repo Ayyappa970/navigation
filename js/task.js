6b47beab2312ebcae98119fc09ed6ee5da380529
Ayyappa970 <129055173+Ayyappa970@users.noreply.github.com>
2023-09-05 15:44:04 +0530

    Add files via upload

---
+++ js/task.js
@@ -0,0 +1,9 @@
+var txt =true;
+        function fun(){
+            if(txt){
+                document.getElementById("main").style.display="block"
+            }
+        }
+        function fun1(){
+            document.getElementById("main").style.display="none";
+        }
